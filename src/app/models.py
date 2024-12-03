5151665e83bbbd98d2fee78bdab8748e6e71f5fc
Renji Zhao <rzhao@msudenver.edu>
2024-12-02 18:36:46 -0700

    updates

--- src/app/models.py
+++ src/app/models.py
@@ -27,7 +27,7 @@ class Major(db.Model):
 class Student(db.Model):  
     __tablename__ = 'students' 
     enrollment_date = db.Column(db.Date, nullable=False, default=date.today)
-    student_id = db.Column(db.String, primary_key=True)  
+    student_id = db.Column(db.Integer, primary_key=True)  
     student_name = db.Column(db.String, nullable=False)  
     academic_year = db.Column(db.String)  
     total_gpa = db.Column(db.Float, default=0.0)  
