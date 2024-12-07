54a50faa10b0a32cc133ecc67c4a9aeb08ed369d
Kayleen Gallegos <kayleengallegos5.kg@gmail.com>
2024-12-06 23:48:08 -0700

    fixed pep8 formatting

--- testing/white_box.py
+++ testing/white_box.py
@@ -1,11 +1,11 @@
-from app.models import Student
 import unittest
 import sys
 import os
-
+ 
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..', 'src')))
-
+ 
+from app.models import Student
 
 class StudentTestCase(unittest.TestCase):
 
